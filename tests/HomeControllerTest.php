9dcc188bf652e7cd86c68f71c9601acf086ea5fd
Titouan Galopin <galopintitouan@gmail.com>
2020-03-13 18:06:10 +0100

    Add test suite

---
+++ tests/HomeControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testSomething()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'En première ligne');
+    }
+}
