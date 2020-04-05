49bd46ea6e39402e9f6d830feafc9b5454836154
Steve Winter <steve@msdev.co.uk>
2020-04-05 13:48:00 +0100

    Begin the process of internationalisation

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -10,12 +11,23 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ContentController extends AbstractController
 {
+    /** @var string */
+    protected $folder;
+
+    public function __construct(string $locale)
+    {
+        $this->folder = 'fr' === $locale ? '' : $locale .'/';
+    }
+
     /**
-     * @Route("/comment-ca-marche", name="content_how")
+     * @Route({
+     *     "fr": "/comment-ca-marche",
+     *     "en": "/how-this-works"
+     * }, name="content_how")
      */
     public function how()
     {
-        return $this->render('content/how.html.twig');
+        return $this->render('content/' . $this->folder . 'how.html.twig');
     }
 
     /**
@@ -23,54 +35,72 @@ class ContentController extends AbstractController
      */
     public function international()
     {
-        return $this->render('content/international.html.twig');
+        return $this->render('content/' . $this->folder . 'international.html.twig');
     }
 
     /**
-     * @Route("/pourquoi", name="content_why")
+     * @Route({
+     *     "fr": "/pourquoi",
+     *     "en": "/why"
+     * }, name="content_why")
      */
     public function why()
     {
-        return $this->render('content/why.html.twig');
+        return $this->render('content/' . $this->folder . 'why.html.twig');
     }
 
     /**
-     * @Route("/qui-sommes-nous", name="content_who")
+     * @Route({
+     *     "fr": "/qui-sommes-nous",
+     *     "en": "/who-are-we"
+     * }, name="content_who")
      */
     public function who()
     {
-        return $this->render('content/who.html.twig');
+        return $this->render('content/' . $this->folder . 'who.html.twig');
     }
 
     /**
-     * @Route("/politique-de-confidentialite", name="content_privacy")
+     * @Route({
+     *     "fr": "/politique-de-confidentialite",
+     *     "en": "/privacy-policy"
+     * }, name="content_privacy")
      */
     public function privacy()
     {
-        return $this->render('content/privacy.html.twig');
+        return $this->render('content/' . $this->folder . 'privacy.html.twig');
     }
 
     /**
-     * @Route("/conditions-d-utilisation", name="content_conditions")
+     * @Route({
+     *     "fr": "/conditions-d-utilisation",
+     *     "en": "/terms-of-use"
+     * }, name="content_conditions")
      */
     public function conditions()
     {
-        return $this->render('content/conditions.html.twig');
+        return $this->render('content/' . $this->folder . 'conditions.html.twig');
     }
 
     /**
-     * @Route("/mentions-legales", name="content_legalities")
+     * @Route({
+     *     "fr": "/mentions-legales",
+     *     "en": "/legal-statement"
+     * }, name="content_legalities")
      */
     public function legalities()
     {
-        return $this->render('content/legalities.html.twig');
+        return $this->render('content/' . $this->folder . 'legalities.html.twig');
     }
 
     /**
-     * @Route("/informations-officielles", name="content_links")
+     * @Route({
+     *     "fr": "/informations-officielles",
+     *     "en": "/official-information"
+     * }, name="content_links")
      */
     public function links()
     {
-        return $this->render('content/links.html.twig');
+        return $this->render('content/' . $this->folder . 'links.html.twig');
     }
 }
